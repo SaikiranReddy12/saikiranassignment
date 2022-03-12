ad35fc162526e4ceb8149650ea71550da4f96be3
SaikiranReddy12 <101470045+SaikiranReddy12@users.noreply.github.com>
2022-03-13 00:32:22 +0530

    Create Gradeaverage.java

---
+++ Gradeaverage.java
@@ -0,0 +1,34 @@
+package Grade;
+import java.util.Scanner;
+public class GradeAverage {
+public static void main(String[] args)
+{
+  Scanner scanner=new Scanner(System.in);
+  int numStudents;
+  int marks;
+  System.out.println("Enter the number of students");
+  numStudents= scanner.nextInt();
+  double average=0.0;
+  double total=0.0;
+  int grades[]=new int[numStudents];
+  int counter=0;
+  while(counter<numStudents)
+  {
+    System.out.println("Enter the grade for student "+ (counter+1));
+    marks=scanner.nextInt();
+    if(marks>100 || marks<0 )
+    {
+      System.out.println("Invalid grade, try again");
+      continue;
+    }
+    grades[counter++]= marks;
+  }
+  for(int grade: grades)
+  {
+    total+=grade;
+  }
+  System.out.println("total marks are : "+ total);
+  average=total/grades.length;
+  System.out.println("Average : "+ average);
+ }
+}
